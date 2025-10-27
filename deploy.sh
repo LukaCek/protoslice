f9e7079b25e8e7f907ea0285163f6cba60aa6248
LukaCek <cek.luka10@gmail.com>
2025-10-27 21:26:12 +0100

    first

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+APP_NAME="protoprice"
+PORT="5252"
+
+docker build -t $APP_NAME:latest . && \
+docker stop $APP_NAME 2>/dev/null || true && \
+docker rm $APP_NAME 2>/dev/null || true && \
+docker run -d --name $APP_NAME -p $PORT:$PORT $APP_NAME:latest
